83f4cec028d67371cbdd4a82a110bc6c2bc32a6d
Gustavo Silva <silva95gustavo@gmail.com>
2016-04-26 10:23:58 +0100

    Started structuring files.

---
+++ js/Converter.js
@@ -0,0 +1,11 @@
+Converter.prototype = Object.create(Converter.prototype);
+Converter.prototype.constructor = Converter;
+
+function Converter(fa) {
+	this.fa = fa;
+}
+
+Converter.prototype.convert = function() {
+	// TODO
+	// return newFa;
+}
