2eb12401f811e2ed159968186c9834c443d0fa5b
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-10 16:28:46 +0100

    Started CST listener

---
+++ dsl/CustomListener.js
@@ -0,0 +1,87 @@
+// Generated from dsl/EliminationOrder.g4 by ANTLR 4.5.3
+// jshint ignore: start
+var EliminationOrderListener = require('dsl/EliminationOrderListener');
+
+// This class defines a complete listener for a parse tree produced by EliminationOrderParser.
+function CustomListener() {
+	EliminationOrderListener.EliminationOrderListener.call(this);
+	return this;
+}
+
+CustomListener.prototype = Object.create(EliminationOrderListener.EliminationOrderListener.prototype);
+CustomListener.prototype.constructor = CustomListener;
+
+// Enter a parse tree produced by EliminationOrderParser#s.
+CustomListener.prototype.enterS = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#s.
+CustomListener.prototype.exitS = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#manual.
+CustomListener.prototype.enterManual = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#manual.
+CustomListener.prototype.exitManual = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#auto.
+CustomListener.prototype.enterAuto = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#auto.
+CustomListener.prototype.exitAuto = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#type.
+CustomListener.prototype.enterType = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#type.
+CustomListener.prototype.exitType = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#e.
+CustomListener.prototype.enterE = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#e.
+CustomListener.prototype.exitE = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#e1.
+CustomListener.prototype.enterE1 = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#e1.
+CustomListener.prototype.exitE1 = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#loop.
+CustomListener.prototype.enterLoop = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#loop.
+CustomListener.prototype.exitLoop = function(ctx) {
+};
+
+
+// Enter a parse tree produced by EliminationOrderParser#v.
+CustomListener.prototype.enterV = function(ctx) {
+};
+
+// Exit a parse tree produced by EliminationOrderParser#v.
+CustomListener.prototype.exitV = function(ctx) {
+};
+
+
+
+exports.CustomListener = CustomListener;
--- js/DSLLoader.js
+++ js/DSLLoader.js
@@ -9,12 +9,17 @@ DSLLoader.prototype.load = function() {
 	var antlr4 = require('antlr4/index');
 	var EliminationOrderLexer = require('dsl/EliminationOrderLexer');
 	var EliminationOrderParser = require('dsl/EliminationOrderParser');
+	var CustomListener = require('dsl/CustomListener');
 	var chars = new antlr4.InputStream(this.dslString);
 	var lexer = new EliminationOrderLexer.EliminationOrderLexer(chars);
 	var tokens  = new antlr4.CommonTokenStream(lexer);
 	var parser = new EliminationOrderParser.EliminationOrderParser(tokens);
+	var listener = new CustomListener.CustomListener();
 	parser.buildParseTrees = true;
 	var tree = parser.s();
+	antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
+	
+	
 	
 	// TODO AST
 	// return ast;
