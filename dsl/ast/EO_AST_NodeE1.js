7529102def2febe215e498ca7018b4541a01cbab
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-23 03:30:21 +0100

    E1 eval

--- dsl/ast/EO_AST_NodeE1.js
+++ dsl/ast/EO_AST_NodeE1.js
@@ -20,7 +20,24 @@ EO_AST_NodeE1.prototype = Object.create(EO_AST_Node.EO_AST_Node.prototype);
 EO_AST_NodeE1.prototype.constructor = EO_AST_NodeE1;
 
 EO_AST_NodeE1.prototype.eval = function(graph, result) {
-	// TODO implement
+	this.children[1].eval(graph, result);
+	if(this.children.length === 2) {	// EO_AST_NodeE1 not present
+		return;
+	}
+	
+	var temp_result = new EvalResult();
+	temp_result.init(graph.nodes.length);
+	this.children[2].eval(temp_result);
+	
+	var type = this.children[1].children[0];
+	switch(type) {
+	case EO_AST_NodeE1.Type.PLUS:
+		result.operation(temp_result.getScores(), EvalResult.Operation.ADD);
+		break;
+	case EO_AST_NodeE1.MINUS:
+		result.operation(temp_result.getScores(), EvalResult.Operation.SUB);
+		break;
+	}
 }
 
 exports.EO_AST_NodeE1 = EO_AST_NodeE1;
