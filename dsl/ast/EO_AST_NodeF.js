1f73f3b7fc238627854b7e1332cc6c37b21caf4b
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-23 03:42:50 +0100

    F eval

--- dsl/ast/EO_AST_NodeF.js
+++ dsl/ast/EO_AST_NodeF.js
@@ -1,4 +1,7 @@
 var EO_AST_Node = require('dsl/ast/EO_AST_Node');
+var EO_AST_NodeV = require('dsl/ast/EO_AST_NodeV').EO_AST_NodeV;
+var EO_AST_NodeLoop = require('dsl/ast/EO_AST_NodeLoop').EO_AST_NodeLoop;
+var EO_AST_NodeE = require('dsl/ast/EO_AST_NodeE').EO_AST_NodeE;
 
 /*
  * Usage example:
@@ -17,7 +20,20 @@ EO_AST_NodeF.prototype = Object.create(EO_AST_Node.EO_AST_Node.prototype);
 EO_AST_NodeF.prototype.constructor = EO_AST_NodeF;
 
 EO_AST_NodeF.prototype.eval = function(graph, result) {
-	// TODO implement
+	var child = this.children[0];
+	if(child instanceof EO_AST_NodeV) {
+		child.eval(graph, result);
+	} else if(child instanceof EO_AST_NodeLoop) {
+		child.eval(graph, result);
+	} else if(child instanceof EO_AST_NodeE) {
+		child.eval(graph, result);
+	} else {
+		var score = [];
+		for(var i = 0; i < graph.nodes.length; ++i) {
+			score.push(child);
+		}
+		result.setScores(score);
+	}
 }
 
 exports.EO_AST_NodeF = EO_AST_NodeF;
