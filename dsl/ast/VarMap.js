fe7c5289d0276419b6c299edd10475132fa57857
aslmbc13 <asl_mbc_13@hotmail.com>
2016-05-31 21:27:44 +0100

    VarMap class

---
+++ dsl/ast/VarMap.js
@@ -0,0 +1,33 @@
+function VarMap() {
+	
+	this.map = [];
+	
+	return this;
+}
+
+VarMap.prototype.constructor = VarMap;
+
+VarMap.prototype.getVar = function(name) {
+	if(typeof name == 'undefined') {
+		return;
+	}
+	
+	return this.map[name];
+};
+
+VarMap.prototype.clone = function() {
+	var cloned = new VarMap();
+	
+	for(var key in this.map) {
+		cloned.map[key] = this.map[key];
+	}
+	
+	return cloned;
+}
+
+VarMap.prototype.addVar = function(key, value) {
+	this.map[key] = value;
+}
+
+
+exports.VarMap = VarMap;
