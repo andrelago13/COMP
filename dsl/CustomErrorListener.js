e7ad28e2431cdc463fc16bdca5313682b9f482d3
aslmbc13 <asl_mbc_13@hotmail.com>
2016-06-04 04:57:10 +0100

    Forgotten file

---
+++ dsl/CustomErrorListener.js
@@ -0,0 +1,21 @@
+var ConsoleErrorListener = require('antlr4/error/ErrorListener').ConsoleErrorListener;
+
+function CustomErrorListener(father) {
+	ConsoleErrorListener.call(this, father);
+	
+	this.hasError = false;
+	this.errorMessage = "";
+	
+	return this;
+}
+
+CustomErrorListener.prototype = Object.create(ConsoleErrorListener.prototype);
+CustomErrorListener.prototype.constructor = CustomErrorListener;
+
+CustomErrorListener.prototype.syntaxError = function(recognizer, offendingSymbol, line, column, msg, e) {
+	this.hasError = true;
+	this.errorMessage = "Syntax error: line " + line + ":" + column + " " + msg;
+	console.error(this.errorMessage);
+};
+
+exports.CustomErrorListener = CustomErrorListener;
