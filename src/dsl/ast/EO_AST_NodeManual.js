8db894a22169c6034fe79736ee6579bb14da2168
aslmbc13 <asl_mbc_13@hotmail.com>
2016-06-06 21:23:58 +0100

    fix

--- src/dsl/ast/EO_AST_NodeManual.js
+++ src/dsl/ast/EO_AST_NodeManual.js
@@ -20,6 +20,53 @@ EO_AST_NodeManual.prototype.constructor = EO_AST_NodeManual;
 EO_AST_NodeManual.prototype.eval = function(graph, result, vars) {
 	result.init(graph.nodes.length);
 	result.setType(EvalResult.Type.STATIC);
+	
+	if(this.children.length != graph.nodes.length) {
+		var str = "";
+		for(var i = 0; i < graph.nodes.length - 1; ++i) {
+			str += "" + i + ", ";
+		}
+		str += "" + (graph.nodes.length-1);
+		
+		errors.push({
+			offendingSymbol: str,
+			msg: "Manual node must have exactly as many node indexes as graph nodes.",
+		});
+		return;
+	}
+	
+	var indexes = [];
+	for(var i = 0; i < graph.nodes.length - 1; ++i) {
+		indexes[i] = 0;
+	}
+	
+	for(var i = 0; i < this.children.length; ++i) {
+		if(this.children[i] < graph.nodes.length) {
+			indexes[this.children[i]]++;
+		}
+	}
+	
+	var problem = false;
+	for(var i = 0; i < indexes.length - 1; ++i) {
+		if(indexes[i] < 1) {
+			problem = true;
+			break;
+		}
+	}
+	if(problem) {
+		var str = "";
+		for(var i = 0; i < graph.nodes.length - 1; ++i) {
+			str += "" + i + ", ";
+		}
+		str += "" + (graph.nodes.length-1);
+		
+		errors.push({
+			offendingSymbol: str,
+			msg: "Manual node must have exactly as many node indexes as graph nodes, and specify all indexes from 0 to the last node.",
+		});
+		return;
+	}
+	
 	result.setOrder(this.children);
 }
 
