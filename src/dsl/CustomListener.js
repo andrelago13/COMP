7257adf2fc183261c4ea7f1942a05720ca467b8a
aslmbc13 <asl_mbc_13@hotmail.com>
2016-06-06 19:00:28 +0100

    errors

--- src/dsl/CustomListener.js
+++ src/dsl/CustomListener.js
@@ -301,7 +301,10 @@ CustomListener.prototype.enterV = function(ctx) {
 		node = new EO_AST_NodeV(this.stack.top(), major, minor);
 		console.log(this.getTabbing() + '\t' + "Parsing V \"" + major + "." + minor + "\".");
 		if(minor != "weight") {
-			errors.push("Unexpected MINOR atribute \"" + minor + "\ at \"" + major + "." + minor + "\". Must be \"weight\".");
+			errors.push({
+				offendingSymbol: minor,
+				msg: "Unexpected MINOR atribute \"" + minor + "\ at \"" + major + "." + minor + "\". Must be \"weight\".",
+			});
 		}
 	} else {
 		// MAJOR
