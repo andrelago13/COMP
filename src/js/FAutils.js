360e0775c9d9d22c53e37bc753173a47304227c6
aslmbc13 <asl_mbc_13@hotmail.com>
2016-08-18 23:45:34 +0100

    [YUIdoc] FAutils.js

--- src/js/FAutils.js
+++ src/js/FAutils.js
@@ -1,23 +1,38 @@
 var EPSILON = '\u03B5';
 
-/*
- * This file as some useful tools used for finite automata manipulation
+/**
+ * Useful methods for finite automata manipulation. To see the available methods
+ * please consult the file, all the methods are documented there.
+ * 
+ * @module FAutils
  */
 
+/*
+ * Ways to measure the quality of a Regular Expression
+ */
 Metrics = {
-	numSymbols : 0,
-	length : 1
+	numSymbols : 0, // Number of symbol characters (excludes parenthesis, commas, ...)
+	length : 1		// Actual length of the RE string
 }
 
+/*
+ * Returns true if the node is an acceptance node (using "vis" criteria)
+ */
 function isNodeFinal(node) {
 	return node.peripheries === 2 || node.shape === "doublecircle"
 }
 
+/*
+ * Gives a node the visual properties of a not final node (for use with "vis")
+ */
 function makeNodeNotFinal(node) {
 	node.peripheries = 1;
 	node.shape = "circle";
 }
 
+/*
+ * Returns a node from the FA "fa", with the given id
+ */
 function findNodeByID(fa, id) {
 	for (var i = 0; i < fa.nodes.length; i++) {
 		if (fa.nodes[i].id === id)
@@ -26,10 +41,21 @@ function findNodeByID(fa, id) {
 	console.error('Node "' + id + '" not found.');
 }
 
+/*
+ * Returns a random string with the given length (for example, for hashing)
+ */
 function randomString(length) {
 	return Array(length+1).join((Math.random().toString(36)+'00000000000000000').slice(2, 18)).slice(0, length);
 }
 
+/*
+ * Adds an edge to the FA "fa"
+ *  - "from" is the source node
+ *  - "to" is the destination node
+ *  - "label" is the edge's label (used in the RE)
+ *  
+ *  The created edge is assigned a random id (hash)
+ */
 function addEdge(fa, from, to, label) {
 	for (var i = 0; i < fa.edges.length; i++) {
 		if (fa.edges[i].fromID === from && fa.edges[i].toID === to) {
@@ -51,11 +77,17 @@ function addEdge(fa, from, to, label) {
 	fa.edges.push(edge);
 }
 
+/*
+ * Removes the edge with id "edgeID" from the FA "fa"
+ */
 function removeEdge(fa, edgeID) {
 	fa.edges.splice(edgeID, 1);
 	addHelpfulInfoToNodesAndEdges(fa);
 }
 
+/*
+ * Removes the node with id "nodeID" from the FA "fa"
+ */
 function removeNode(fa, nodeID) {
 	fa.nodes.splice(nodeID, 1);
 	for (var i = 0; i < fa.edges.length; i++) {
@@ -69,7 +101,18 @@ function removeNode(fa, nodeID) {
 }
 
 /*
- * Helpful information is all sorts of node data that can be used for node evaluation, such as ins, outs, ins_nl, outs_nl, ...
+ * Adds some helpful information to the nodes and edges of an FA.
+ * 
+ * Helpful information is all sorts of node data that can be used for node evaluation:
+ *  - ins - edges to which the node is the destination
+ *  - outs - edges to which the node is the source
+ *  - ins_nl - not looping "in" edges (in edges to which the node is not the source)
+ *  - outs_nl - not looping "out" edges (out edges to which the node is not the destination)
+ *  - indeg - number of elements in "ins"
+ *  - outdeg - number of elements in "outs"
+ *  - indeg_nl - number of elements in "ins_nl"
+ *  - outdeg_nl - number of elements in "outs_nl"
+ *  - nloops - number of loops in the node (edges to which the node is both source and destination)
  */
 function addHelpfulInfoToNodesAndEdges(data) {
 	for (var i = 0; i < data.nodes.length; i++) {
@@ -142,10 +185,18 @@ function addHelpfulInfoToNodesAndEdges(data) {
 	}
 }
 
+/*
+ * Clones the FA given
+ */
 function FAClone(fa) {
 	return jQuery.extend(true, {}, fa);
 }
 
+/*
+ * Calculates the weight of a regular expression given a certain metric.
+ * 
+ * The metric must be a value in the Metrics enum above.
+ */
 function calculateWeight(regex, metric) {
 	if(typeof metric == 'undefined') {
 		metric = Metrics.numSymbols;
