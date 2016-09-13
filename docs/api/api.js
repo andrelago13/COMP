5dde89dec793cabf8f8d65348ddd8b6c14cc93a4
aslmbc13 <asl_mbc_13@hotmail.com>
2016-09-13 22:45:28 +0100

    Added last version of documentation.

---
+++ docs/api/api.js
@@ -0,0 +1,61 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Converter",
+        "CustomErrorListener",
+        "DSLLoader",
+        "EO_AST",
+        "EO_AST_Node",
+        "EO_AST_NodeAuto",
+        "EO_AST_NodeE",
+        "EO_AST_NodeE1",
+        "EO_AST_NodeF",
+        "EO_AST_NodeLoop",
+        "EO_AST_NodeManual",
+        "EO_AST_NodeReserved",
+        "EO_AST_NodeT",
+        "EO_AST_NodeT1",
+        "EO_AST_NodeTerminal",
+        "EO_AST_NodeV",
+        "EvalResult",
+        "FALoader",
+        "Stack",
+        "VarMap"
+    ],
+    "modules": [
+        "FAutils",
+        "FiniteAutomata",
+        "Grammar",
+        "Main",
+        "Utils"
+    ],
+    "allModules": [
+        {
+            "displayName": "FAutils",
+            "name": "FAutils",
+            "description": "Useful methods for finite automata manipulation. To see the available methods\nplease consult the file, all the methods are documented there."
+        },
+        {
+            "displayName": "FiniteAutomata",
+            "name": "FiniteAutomata",
+            "description": "Module containing utilities for manipulating finite automatas"
+        },
+        {
+            "displayName": "Grammar",
+            "name": "Grammar",
+            "description": "Classes to help parsing, validating and evaluating expressions under the DSL we defined in the \"About\" page"
+        },
+        {
+            "displayName": "Main",
+            "name": "Main",
+            "description": "This module initializes the page and performs the on-page actions,\nthen calling the rest of the API to perform the intended actions.\nPlease consult the documentation of the methods in this file for more details"
+        },
+        {
+            "displayName": "Utils",
+            "name": "Utils",
+            "description": "Utilities for the application"
+        }
+    ],
+    "elements": []
+} };
+});
